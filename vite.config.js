31977f4bc376a5f0b4f3672fa9405ca9a2efc612
Ana Clara <anacmm2@al.insper.edu.br>
2025-10-20 09:09:50 -0300

    fazendo deploy

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: ['projeto-2-frontend-anaclara.onrender.com']
+  }
 })
